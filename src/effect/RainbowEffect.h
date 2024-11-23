1264eeab1405ed4d72bbf7aa3eee8dd44882da77
ktabakov <konkov90@gmail.com>
2024-11-23 21:22:51 +0200

    SparkleEffect

--- src/effect/RainbowEffect.h
+++ src/effect/RainbowEffect.h
@@ -2,10 +2,14 @@
 #define RAINBOW_EFFECT_H
 
 #include "Effect.h"
+#include <Ticker.h>
 
 class RainbowEffect : public Effect {
 private:
     uint8_t hue = 0;
+    Ticker effectTimer;
+
+    void internalRun(CRGB* leds, int numLeds);
 
 public:
     void run(CRGB* leds, int numLeds) override;
